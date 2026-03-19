9019bb68620509d318a57a00d5a56b7e35475629
Eric <eric032807@gmail.com>
2026-03-19 10:56:33 -0400

    added AI direction

--- assignment2_101559700.py
+++ assignment2_101559700.py
@@ -49,11 +49,14 @@ class NetworkTool:
 
 # Q3: What is the benefit of using @property and @target.setter?
 # TODO: Your 2-4 sentence answer here... (Part 2, Q3)
+#The benefit of using @porperty lets you read an attribute like a normal variable, but secretly runs a method behind the scenes
+#The benefit of using @target.setter, you can validate the value before saving it.
 
 
 # Q1: How does PortScanner reuse code from NetworkTool?
 # TODO: Your 2-4 sentence answer here... (Part 2, Q1)
-
+# PortScanner inherits from NetworkTool by defining it as class PortScanner(NetworkTool), This mean all methods and attributes from 
+#NetworkTool are automatically available in PortScanner.
 
 # TODO: Create the PortScanner child class that inherits from NetworkTool (Step vi)
 # - Constructor: call super().__init__(target), initialize self.scan_results = [], self.lock = threading.Lock()
@@ -72,6 +75,7 @@ class PortScanner (NetworkTool):
 
 #     Q4: What would happen without try-except here?
 #     TODO: Your 2-4 sentence answer here... (Part 2, Q4)
+#     Without using try-except, if an error occurs Python will throw an unhandlled exception and crash the program.
 #
 #     - try-except with socket operations
 #     - Create socket, set timeout, connect_ex
@@ -105,6 +109,8 @@ def get_open_ports(self):
 
 #     Q2: Why do we use threading instead of scanning one port at a time?
 #     TODO: Your 2-4 sentence answer here... (Part 2, Q2)
+#     Scanning one port at a time means the program must wait for each connection responce before moving on which is very slow.
+#     With threading, multiple ports are scanned at the same time, so they don't block each other. 
 #
 # - scan_range(self, start_port, end_port):
 #     - Create threads list
