76218ee3397354619ce322e5f33e0fb2b6d5842d
Eric <eric032807@gmail.com>
2026-03-19 17:14:09 -0400

    finished assignment2.py

--- assignment2_101559700.py
+++ assignment2_101559700.py
@@ -33,6 +33,11 @@ class NetworkTool:
     def __init__(self,target):
         self.__target = target
     
+# Q3: What is the benefit of using @property and @target.setter?
+# TODO: Your 2-4 sentence answer here... (Part 2, Q3)
+#The benefit of using @porperty lets you read an attribute like a normal variable, but secretly runs a method behind the scenes
+#The benefit of using @target.setter, you can validate the value before saving it.
+
     @property
     def target(self):
         return self.__target
@@ -47,10 +52,6 @@ class NetworkTool:
     def __del__(self):
         print("NetworkTool instance destroyed")
 
-# Q3: What is the benefit of using @property and @target.setter?
-# TODO: Your 2-4 sentence answer here... (Part 2, Q3)
-#The benefit of using @porperty lets you read an attribute like a normal variable, but secretly runs a method behind the scenes
-#The benefit of using @target.setter, you can validate the value before saving it.
 
 
 # Q1: How does PortScanner reuse code from NetworkTool?
@@ -72,11 +73,6 @@ class PortScanner (NetworkTool):
         super().__del__()
 
 # - scan_port(self, port):
-
-#     Q4: What would happen without try-except here?
-#     TODO: Your 2-4 sentence answer here... (Part 2, Q4)
-#     Without using try-except, if an error occurs Python will throw an unhandlled exception and crash the program.
-#
 #     - try-except with socket operations
 #     - Create socket, set timeout, connect_ex
 #     - Determine Open/Closed status
@@ -84,28 +80,31 @@ class PortScanner (NetworkTool):
 #     - Acquire lock, append (port, status, service_name) tuple, release lock
 #     - Close socket in finally block
 #     - Catch socket.error, print error message
-def scan_port(self, port):
-        try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(1)
-            result = sock.connect_ex((self.target, port))
-            if result == 0 :
-                status = "Open"
-            else:
-                status = "Closed"
-            service_name = common_ports.get(port, "Unknown")
-            self.lock.acquire()
-            self.scan_results.append((port, status, service_name))
-            self.lock.release()
-        except socket.error as e:
-            print(f"Error scanning port {port}: {e}")
-        finally:
-            sock.close()
+    def scan_port(self, port):
+#     Q4: What would happen without try-except here?
+#     TODO: Your 2-4 sentence answer here... (Part 2, Q4)
+#     Without using try-except, if an error occurs Python will throw an unhandlled exception and crash the program.
+            try:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.settimeout(1)
+                result = sock.connect_ex((self.target, port))
+                if result == 0 :
+                    status = "Open"
+                else:
+                    status = "Closed"
+                service_name = common_ports.get(port, "Unknown")
+                self.lock.acquire()
+                self.scan_results.append((port, status, service_name))
+                self.lock.release()
+            except socket.error as e:
+                print(f"Error scanning port {port}: {e}")
+            finally:
+                sock.close()
 
 # - get_open_ports(self):
 #     - Use list comprehension to return only "Open" results
-def get_open_ports(self):
-    return[result for result in self.scan_results if result[1] == "Open"]
+    def get_open_ports(self):
+        return[result for result in self.scan_results if result[1] == "Open"]
 
 #     Q2: Why do we use threading instead of scanning one port at a time?
 #     TODO: Your 2-4 sentence answer here... (Part 2, Q2)
@@ -117,16 +116,16 @@ def get_open_ports(self):
 #     - Create Thread for each port targeting scan_port
 #     - Start all threads (one loop)
 #     - Join all threads (separate loop)
-def scan_range(self, start_port, end_port):
-    threads = []
-    for port in range(start_port, end_port+1):
-        t = threading.Thread(target=self.scan_port, args=(port,))
-        threads.append(t)
+    def scan_range(self, start_port, end_port):
+        threads = []
+        for port in range(start_port, end_port+1):
+            t = threading.Thread(target=self.scan_port, args=(port,))
+            threads.append(t)
 
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
 
 # TODO: Create save_results(target, results) function (Step vii)
 # - Connect to scan_history.db
@@ -161,20 +160,59 @@ def save_results(target, results):
 # - Print each row in readable format
 # - Handle missing table/db: print "No past scans found."
 # - Close connection
-
+def load_past_scans():
+    try:
+        conn = sqlite3.connect("scan_history.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM scans")
+        rows = cursor.fetchall()
+        if not rows:
+            print(f"No past scans found.")
+        else:
+            for row in rows:
+                print(f"[{row[5]}] {row[1]} : Port {row[2]} ({row[4]}) - {row[3]}")
+    except:
+        print(f"No past scans found.")
+    finally:
+        conn.close()
 
 # ============================================================
 # MAIN PROGRAM
 # ============================================================
 if __name__ == "__main__":
-    pass
     # TODO: Get user input with try-except (Step ix)
     # - Target IP (default "127.0.0.1" if empty)
     # - Start port (1-1024)
     # - End port (1-1024, >= start port)
     # - Catch ValueError: "Invalid input. Please enter a valid integer."
     # - Range check: "Port must be between 1 and 1024."
-
+    target = input("Enter target IP: ") or "127.0.0.1"
+    try:
+        start_port = int(input("Enter start port number: Between 1-1024 : "))
+        end_port = int(input("Enter end port number: Between 1-1024 and need to greater than start port : "))
+        if start_port<1 or start_port >1024 or end_port<1 or end_port > 1024:
+            print("Port must be between 1 and 1024.")
+        elif end_port < start_port:
+            print("End port must be >= start port.")
+        else:
+            scanner = PortScanner(target)
+            print (f"Scanning {target} from port {start_port} to {end_port}...")
+            scanner.scan_range(start_port,end_port)
+            open_port = scanner.get_open_ports()
+            print(f"--- Scan Results for {target} ---")
+            for result in open_port:
+                print (f"Port {result[0]}: {result[1]} ({result[2]})")
+            print ("-------------------")
+            print (f"Total open ports found {len(open_port)}")
+            save_results(target, open_port)
+            answer = input("Would you like to see past scan history? (yes/no)")
+            if answer == "yes" or answer == "YES":
+                load_past_scans()
+            else:
+                pass
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+    
     # TODO: After valid input (Step x)
     # - Create PortScanner object
     # - Print "Scanning {target} from port {start} to {end}..."
@@ -184,8 +222,7 @@ if __name__ == "__main__":
     # - Call save_results()
     # - Ask "Would you like to see past scan history? (yes/no): "
     # - If "yes", call load_past_scans()
-
-
+    
 # Q5: New Feature Proposal
 # TODO: Your 2-3 sentence description here... (Part 2, Q5)
 # Diagram: See diagram_studentID.png in the repository root
